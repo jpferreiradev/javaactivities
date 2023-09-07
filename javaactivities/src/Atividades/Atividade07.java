14d52793c0c98df596f5bf7d78d50eab68e08977
JP Ferreira <jotepauder01@gmail.com>
2023-09-07 11:03:52 -0300

    Update atividades

--- javaactivities/src/Atividades/Atividade07.java
+++ javaactivities/src/Atividades/Atividade07.java
@@ -12,11 +12,11 @@ public class Atividade07 {
         System.out.println("Digite a temperatura:");
         temperatura = scan.nextDouble();
 
-        if( temperatura < -20 ){
+        if( temperatura < 0 ){
             System.out.println("Hipotermia");
         } else if (temperatura > 20 && temperatura < 37.5) {
             System.out.println("Temperatura normal");
-        } else if (temperatura > 37.5 && temperatura < 42) {
+        } else if (temperatura >= 37.5 && temperatura < 42) {
             System.out.println("Febre");
         } else if (temperatura >= 43) {
             System.out.println("Perigooo!");
