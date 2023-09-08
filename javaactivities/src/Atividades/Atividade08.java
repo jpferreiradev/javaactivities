a725d77f1a009d303ebc6860088e2a946d762a1b
JP Ferreira <jotepauder01@gmail.com>
2023-09-08 19:22:52 -0300

    Update atividades

--- javaactivities/src/Atividades/Atividade08.java
+++ javaactivities/src/Atividades/Atividade08.java
@@ -1,4 +1,22 @@
 package Atividades;
 
+import java.util.Scanner;
+
 public class Atividade08 {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        int numeroA;
+        int numeroB;
+        int conversaoNumeros;
+
+        System.out.println("Digite um número A: ");
+        numeroA = scan.nextInt();
+        System.out.println("Digite um número B: ");
+        numeroB = scan.nextInt();
+        System.out.println((int)Math.pow(numeroA,numeroB));
+
+
+
+    }
 }
--- javaactivities/src/Atividades/Atividade10.java
+++ javaactivities/src/Atividades/Atividade10.java
@@ -1,4 +1,38 @@
 package Atividades;
 
+import java.util.Scanner;
+
 public class Atividade10 {
+    public static void main(String[]args){
+
+        // vogais a,e,i,o,u
+        Scanner scan = new Scanner(System.in);
+
+        String letra;
+        String[] vogais = {"a","e","i","o","u"};
+        //int numero;
+
+
+        System.out.println("Digite um letra: ");
+        letra = scan.nextLine();
+        //System.out.println("Digite um número: ");
+        //numero = scan.nextInt();
+
+        //System.out.println("Letra foi: " +"["+ letra +"]" + " e o número foi:" + numero);
+        System.out.println("Letra foi: " +"["+ letra +"]");
+
+
+        //System.out.println(vogais.length);
+        if(letra.equals(vogais.length)){
+            System.out.println("É uma vogal");
+        } else {
+            System.out.println("Não é uma vogal");
+        }
+
+
+
+
+
+
+    }
 }
